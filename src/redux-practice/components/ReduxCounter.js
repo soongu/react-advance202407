b0c5806cc3cbd22ad06d4fc1c5ac61f01aebb98e
soongu <naong526@naver.com>
2024-07-26 09:41:09 +0900

    # 리듀서 함수와 useSelector

--- src/redux-practice/components/ReduxCounter.js
+++ src/redux-practice/components/ReduxCounter.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from './ReduxCounter.module.css';
+import { useSelector } from 'react-redux';
 
 const ReduxCounter = () => {
+
+  // useSelector라는 훅을 통해 redux store에 있는 상태값을 가져옴
+  const counter = useSelector(state => state.counter);
 
   return (
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
-      <div className={styles.value}>-- COUNTER VALUE --</div>
+      <div className={styles.value}>{counter}</div>
+      
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+      
       <button>Toggle Counter</button>
     </main>
   );
